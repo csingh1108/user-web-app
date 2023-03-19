67b23c8bfa4a40ac8a63e4f8c9ff9172589a599a
Charanjot Singh <charanjot118@gmail.com>
2023-03-19 14:45:32 +0000

    inital commit

---
+++ src/app/pages/shoppping-cart/shoppping-cart.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-shoppping-cart',
+  templateUrl: './shoppping-cart.component.html',
+  styleUrls: ['./shoppping-cart.component.css']
+})
+export class ShopppingCartComponent {
+
+}
